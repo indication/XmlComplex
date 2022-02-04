4aaa6a59ea642dacc1115d62115d0b0e02cc0768
in_dow <in_dow@hotmail.com>
2022-02-05 00:46:27 +0900

    Separate methods

---
+++ OptionHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmlComplex
+{
+    static class OptionHelper
+    {
+
+        /// <summary>
+        /// Get option strings from argument array
+        /// </summary>
+        /// <param name="args">argument array</param>
+        /// <returns>Option pair</returns>
+        /// <remarks>
+        /// Start with - value is option and value is separated by =.
+        /// </remarks>
+        static public Dictionary<string, string> GetOptions(string[] args, string[][] transcodes)
+        {
+            return args.Where(w => w.StartsWith("-"))
+                .Select((item, i) => item.TrimStart('-').Split(new[] { '=' }, 2))
+                .Select((k, i) =>
+                {
+                    var key = k[0].ToLower();
+                    var trans = transcodes?.FirstOrDefault(w => w[0] == key);
+                    return new
+                    {
+                        Key = trans != null ? trans[1] : key,
+                        Value = k.Length > 1 ? k[1] : string.Empty,
+                    };
+                })
+                .GroupBy(w => w.Key)
+                .ToDictionary(k => k.Key, v => v.First().Value);
+        }
+        /// <summary>
+        /// Get arguments without options
+        /// </summary>
+        /// <param name="args">Argument array</param>
+        /// <returns>String array</returns>
+        static public IEnumerable<string> GetNoOption(string[] args)
+        {
+            return args.Where(w => !w.StartsWith("-"));
+        }
+
+        /// <summary>
+        /// Get argument helps
+        /// </summary>
+        /// <param name="args">Argument array</param>
+        /// <returns></returns>
+        static public IEnumerable<string> GetHelp(string[][] args)
+        {
+            var sb = new StringBuilder();
+            foreach (var opts in args)
+            {
+                sb.Clear();
+                sb.Append("\t");
+                if (!string.IsNullOrEmpty(opts[0]))
+                {
+                    if (!string.IsNullOrEmpty(opts[2]))
+                        sb.AppendFormat("-{0}={2} ", opts);
+                    else
+                        sb.AppendFormat("-{0} ", opts);
+                }
+                if (!string.IsNullOrEmpty(opts[1]))
+                {
+                    if (!string.IsNullOrEmpty(opts[2]))
+                        sb.AppendFormat("[--{1}={2}]", opts);
+                    else
+                        sb.AppendFormat("[--{1}]", opts);
+                }
+                yield return sb.ToString();
+                foreach (var line in opts.Skip(3))
+                    yield return string.Format("\t\t{0}", line.Replace("\r\n", "\r\n\t\t"));
+                yield return string.Empty;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using XmlComplex.Properties;
 
 namespace XmlComplex
 {
-    class Program
+    partial class Program
     {
         /// <summary>
         /// Main
@@ -19,7 +19,7 @@ namespace XmlComplex
         [STAThread]
         static int Main(string[] args)
         {
-            var _items = GetNoOption(args).ToList();
+            var _items = OptionHelper.GetNoOption(args).ToList();
             //Console.WriteLine(Resources.MessageArguments, string.Join(",", _items));
             var optionParams = new[] {
                 //from, to
@@ -32,122 +32,51 @@ namespace XmlComplex
                 new [] { "v", "version", "", Resources.HelpOptionsVersion },
                 new [] { "h", "help", "", Resources.HelpOptionsHelp },
             };
-            var options = GetOptions(args, optionParams);
+            var options = OptionHelper.GetOptions(args, optionParams);
 
             var newline = Environment.NewLine;
-            do
+            if (options.ContainsKey("newline"))
             {
-                if (options.ContainsKey("newline"))
+                newline = GetNewLineCode(options["newline"]);
+                if (newline == null)
                 {
-                    NewLineKind kind;
-                    if (Enum.TryParse(options["newline"], true, out kind))
-                    {
-                        switch (kind)
-                        {
-                            case NewLineKind.CR:
-                                newline = "\r";
-                                break;
-                            case NewLineKind.LF:
-                                newline = "\n";
-                                break;
-                            case NewLineKind.CRLF:
-                                newline = "\r\n";
-                                break;
-                        }
-                        break;
-
-                    }
                     Console.WriteLine(Resources.ErrorInvalidLineCode, options["newline"]);
                     return -1;
                 }
-            } while (false);
+            }
             if (options.ContainsKey("version"))
             {
-                var asm = Assembly.GetExecutingAssembly();
-                var version = Attribute.GetCustomAttribute(asm, typeof(AssemblyFileVersionAttribute)) as AssemblyFileVersionAttribute;
-                Console.WriteLine(Resources.MessageVersion, Path.GetFileName(asm.Location), version?.Version);
+                ShowVersion();
                 return 0;
             }
             Encoding encode = null;
-            do
+            if (options.ContainsKey("encoding") || options.ContainsKey("encodings"))
             {
-                if (options.ContainsKey("encoding") || options.ContainsKey("encodings"))
+                if (options.ContainsKey("encoding"))
                 {
-                    if (options.ContainsKey("encoding"))
+                    var str = options["encoding"];
+                    try
                     {
-                        var str = options["encoding"];
-                        var hasBe = str.ToLower().Contains("be");
-                        var hasBom = str.ToLower().Contains("bom") && !str.ToLower().Contains("nobom");
-                        if (str.StartsWith("utf-8", StringComparison.CurrentCultureIgnoreCase) || str.StartsWith("utf8", StringComparison.CurrentCultureIgnoreCase))
-                        {
-                            encode = new UTF8Encoding(hasBom);
-                            break;
-                        }
-                        else if (str.StartsWith("utf-32", StringComparison.CurrentCultureIgnoreCase) || str.StartsWith("utf32", StringComparison.CurrentCultureIgnoreCase))
-                        {
-                            encode = new UTF32Encoding(hasBe, hasBom);
-                            break;
-                        }
-                        else if (str.StartsWith("utf-16", StringComparison.CurrentCultureIgnoreCase) || str.StartsWith("utf16", StringComparison.CurrentCultureIgnoreCase))
-                        {
-                            encode = new UnicodeEncoding(hasBe, hasBom);
-                            break;
-                        }
-
-                        try
-                        {
-                            encode = Encoding.GetEncoding(str);
-                            break;
-                        }
-                        catch (ArgumentException ex)
-                        {
-                            Console.WriteLine(Resources.ErrorInvalidEncoding, str, ex.Message);
-                        }
+                        encode = GetEncoding(str);
                     }
-                    Console.WriteLine();
-                    Console.WriteLine(Resources.HelpEncoding);
-                    foreach (var enc in Encoding.GetEncodings().OrderBy(w => w.Name))
+                    catch (ArgumentException ex)
                     {
-                        Console.WriteLine("\t{0}\t{1}", enc.Name, enc.DisplayName);
+                        Console.WriteLine(Resources.ErrorInvalidEncoding, str, ex.Message);
+                        Console.WriteLine();
                     }
-                    Console.WriteLine("\t{0}",Resources.HelpEncodingExt);
-                    return options.ContainsKey("encodings")  ? 0 : -1;
                 }
-            } while (false);
+                if (encode == null || options.ContainsKey("encodings"))
+                {
+                    ShowEncodings();
+                    return options.ContainsKey("encodings") ? 0 : -1;
+                }
+            }
             if (_items.Count < 1 || options.ContainsKey("help") || !options.ContainsKey("output"))
             {
-                var asm = Assembly.GetExecutingAssembly();
-                var copyright = Attribute.GetCustomAttribute(asm, typeof(AssemblyCopyrightAttribute)) as AssemblyCopyrightAttribute;
-                var title = Attribute.GetCustomAttribute(asm, typeof(AssemblyTitleAttribute)) as AssemblyTitleAttribute;
-                Console.WriteLine("{0} ({1})", title?.Title, copyright?.Copyright);
-                var filename = Path.GetFileName(asm.Location);
-                Console.WriteLine(Resources.HelpMessage, filename, Resources.HelpEncodingExt, string.Join("|", Enum.GetNames(typeof(NewLineKind))));
-                foreach (var opts in optionParams)
-                {
-                    Console.Write("\t");
-                    if (!string.IsNullOrEmpty(opts[0]))
-                    {
-                        if (!string.IsNullOrEmpty(opts[2]))
-                            Console.Write("-{0}={2} ", opts);
-                        else
-                            Console.Write("-{0} ", opts);
-                    }
-                    if (!string.IsNullOrEmpty(opts[1]))
-                    {
-                        if (!string.IsNullOrEmpty(opts[2]))
-                            Console.Write("[--{1}={2}]", opts);
-                        else
-                            Console.Write("[--{1}]", opts);
-                    }
-                    Console.WriteLine();
-                    foreach(var line in opts.Skip(3))
-                        Console.WriteLine("\t\t{0}", line.Replace("\r\n", "\r\n\t\t"));
-                    Console.WriteLine();
-                }
-
-                return options.ContainsKey("help")  ? 0 : -1;
+                ShowHelp(optionParams);
+                return options.ContainsKey("help") ? 0 : -1;
             }
-            
+
             bool findAll = true;
             foreach (var _item in _items.Where(w => !File.Exists(w)))
             {
@@ -177,46 +106,88 @@ namespace XmlComplex
                 _basedoc.Save(options["output"]);
             return 0;
         }
-
         /// <summary>
-        /// Get option strings from argument array
+        /// Get new line code
         /// </summary>
-        /// <param name="args">argument array</param>
-        /// <returns>Option pair</returns>
-        /// <remarks>
-        /// Start with - value is option and value is separated by =.
-        /// </remarks>
-        static Dictionary<string, string> GetOptions(string[] args, string[][] transcodes)
+        /// <param name="newLine">name for new line</param>
+        /// <returns>string from names</returns>
+        private static string GetNewLineCode(string newLine)
         {
-            return args.Where(w => w.StartsWith("-"))
-                .Select((item, i) => item.TrimStart('-').Split(new[] { '=' }, 2))
-                .Select((k, i) =>
+            NewLineKind kind;
+            if (Enum.TryParse(newLine, true, out kind))
+            {
+                switch (kind)
                 {
-                    var key = k[0].ToLower();
-                    var trans = transcodes?.FirstOrDefault(w => w[0] == key);
-                    return new
-                    {
-                        Key = trans != null ? trans[1] : key,
-                        Value = k.Length > 1 ? k[1] : string.Empty,
-                    };
-                })
-                .GroupBy(w => w.Key)
-                .ToDictionary(k => k.Key, v => v.First().Value);
-        }
-        class OptionValues
-        {
-            public string Short { get; set; }
-            public string Long { get; set; }
-
+                    case NewLineKind.CR:
+                        return "\r";
+                    case NewLineKind.LF:
+                        return "\n";
+                    case NewLineKind.CRLF:
+                        return "\r\n";
+                }
+            }
+            return null;
         }
         /// <summary>
-        /// Get arguments without options
+        /// Show encodings
         /// </summary>
-        /// <param name="args">Argument array</param>
-        /// <returns>String array</returns>
-        static IEnumerable<string> GetNoOption(string[] args)
+        private static void ShowEncodings()
         {
-            return args.Where(w => !w.StartsWith("-"));
+            Console.WriteLine(Resources.HelpEncoding);
+            foreach (var enc in Encoding.GetEncodings().OrderBy(w => w.Name))
+            {
+                Console.WriteLine("\t{0}\t{1}", enc.Name, enc.DisplayName);
+            }
+            Console.WriteLine("\t{0}", Resources.HelpEncodingExt);
+        }
+        /// <summary>
+        /// Get encoding from param
+        /// </summary>
+        /// <param name="str">encoding name</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">encoding is not found</exception>
+        private static Encoding GetEncoding(string str)
+        {
+            var hasBe = str.ToLower().Contains("be");
+            var hasBom = str.ToLower().Contains("bom") && !str.ToLower().Contains("nobom");
+            if (str.StartsWith("utf-8", StringComparison.CurrentCultureIgnoreCase) || str.StartsWith("utf8", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return new UTF8Encoding(hasBom);
+            }
+            else if (str.StartsWith("utf-32", StringComparison.CurrentCultureIgnoreCase) || str.StartsWith("utf32", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return new UTF32Encoding(hasBe, hasBom);
+            }
+            else if (str.StartsWith("utf-16", StringComparison.CurrentCultureIgnoreCase) || str.StartsWith("utf16", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return new UnicodeEncoding(hasBe, hasBom);
+            }
+
+            return Encoding.GetEncoding(str);
+        }
+        /// <summary>
+        /// Show version
+        /// </summary>
+        private static void ShowVersion()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var version = Attribute.GetCustomAttribute(asm, typeof(AssemblyFileVersionAttribute)) as AssemblyFileVersionAttribute;
+            Console.WriteLine(Resources.MessageVersion, Path.GetFileName(asm.Location), version?.Version);
+        }
+        /// <summary>
+        /// Show help
+        /// </summary>
+        /// <param name="optionParams"></param>
+        private static void ShowHelp(string[][] optionParams)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var copyright = Attribute.GetCustomAttribute(asm, typeof(AssemblyCopyrightAttribute)) as AssemblyCopyrightAttribute;
+            var title = Attribute.GetCustomAttribute(asm, typeof(AssemblyTitleAttribute)) as AssemblyTitleAttribute;
+            Console.WriteLine("{0} ({1})", title?.Title, copyright?.Copyright);
+            var filename = Path.GetFileName(asm.Location);
+            Console.WriteLine(Resources.HelpMessage, filename, Resources.HelpEncodingExt, string.Join("|", Enum.GetNames(typeof(NewLineKind))));
+            foreach (var line in OptionHelper.GetHelp(optionParams))
+                Console.WriteLine(line);
         }
     }
 }
